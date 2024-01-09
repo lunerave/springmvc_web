c0ba279f3c3466f4012787d899330b93f51531d8
lunerave <lunerave07@naver.com>
2024-01-09 21:19:14 +0900

    Code Implementation: RedirectAttributes

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -87,16 +88,25 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(Item item) {
-        // @ModelAttribute 이름 안 넣으면 클래스 첫 글자를 소문자로 바꾼 이름이 자동으로 등록 됨. Item -> item
-        // Item은 임의로 만든 객체이므로 @ModelAttribute 생략 시에도 자동으로 적용됨.
         // Model 역시 생략 가능
 
         itemRepository.save(item);
-        //model.addAttribute("item", item); // @ModelAttribute 사용 시 생략 가능
+        //model.addAttribute("item", item);
 
         return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+
+        Item savedItem = itemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true); // 변수로 없기 때문에 query parameter로 넘어감
+
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
